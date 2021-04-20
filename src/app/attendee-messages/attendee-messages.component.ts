40aad6a532ed004c9130f1b06d944686278d7b6a
Guru Prakash Davala <guru.govindaraj@gmail.com>
2021-04-20 19:48:57 +0100

    dropdown option validation

--- src/app/attendee-messages/attendee-messages.component.ts
+++ src/app/attendee-messages/attendee-messages.component.ts
@@ -17,19 +17,25 @@ export class AttendeeMessagesComponent implements OnInit {
   isNoRecordsAttendee: boolean = false;
   isNoRecordsExhibitor: boolean = false;
   isAttendeeUnAvailable: boolean = false;
+  optionNotSelected: boolean = false;
   attendeeOption: string = "";
 
   ngOnInit() {}
 
   getAllAttendeeMessages() {
     if (!this.attendeeID) {
-      //console.log(isNaN(this.attendeeID));
       this.enableErrorMessage = true;
       return false;
     }
+
     if (isNaN(this.attendeeID)) {
       console.log(isNaN(this.attendeeID));
       this.enableInvalidFormatErrorMessage = true;
+      return false;
+    }
+
+    if (this.attendeeOption == "") {
+      this.optionNotSelected = true;
       return false;
     }
 
@@ -41,102 +47,76 @@ export class AttendeeMessagesComponent implements OnInit {
       this.isNoRecordsExhibitor = false;
       this.enableErrorMessage = false;
       this.enableInvalidFormatErrorMessage = false;
+      this.optionNotSelected = false;
 
       this.service.getAttendeeDetails(this.attendeeID).subscribe((response) => {
         if (response.length == 0) {
+          console.log(response);
           this.isAttendeeUnAvailable = true;
           return false;
         } else {
           this.attendeeDetails = response;
-          console.log(this.attendeeDetails);
+          this.getAllAttendeeMessagesByType();
         }
       });
-
-      switch (this.attendeeOption) {
-        case "0":
-          // console.log("0");
-          this.service
-            .getAllAttendeeMessages(this.attendeeID)
-            .subscribe((response) => {
-              if (response.length == 0) {
-                this.isNoRecordsAttendee = true;
-                return false;
-              } else {
-                this.allAttendeeMsgs = response;
-                console.log(this.allAttendeeMsgs);
-              }
-            });
-          break;
-        case "1":
-          // console.log("1");
-          this.allAttendeeMsgs = [];
-          this.service
-            .getAllConversationBetweenAttendeeAndExhibitors(this.attendeeID)
-            .subscribe((response) => {
-              if (response.length == 0) {
-                this.isNoRecordsExhibitor = true;
-                return false;
-              } else {
-                this.allAttendeeExhibitorMsgs = response;
-              }
-              console.log(response);
-            });
-          break;
-        case "All":
-          // console.log("ALL");
-          this.service
-            .getAllAttendeeMessages(this.attendeeID)
-            .subscribe((response) => {
-              if (response.length == 0) {
-                this.isNoRecordsAttendee = true;
-                return false;
-              } else {
-                this.allAttendeeMsgs = response;
-                console.log(this.allAttendeeMsgs);
-              }
-            });
-
-          this.service
-            .getAllConversationBetweenAttendeeAndExhibitors(this.attendeeID)
-            .subscribe((response) => {
-              if (response.length == 0) {
-                this.isNoRecordsExhibitor = true;
-                return false;
-              } else {
-                this.allAttendeeExhibitorMsgs = response;
-              }
-              console.log(response);
-            });
-          break;
-      }
     }
+  }
 
-    // if (this.attendeeID) {
-    //   console.log(this.attendeeOption);
-    //   this.isNoRecords = false;
-    //   this.enableErrorMessage = false;
-    //   this.service
-    //     .getAllAttendeeMessages(this.attendeeID)
-    //     .subscribe((response) => {
-    //       if (response.length == 0) {
-    //         this.isNoRecords = true;
-    //         return false;
-    //       } else {
-    //         this.allAttendeeMsgs = response;
-    //       }
-    //     });
-    //   if (this.attendeeID) {
-    //     this.service
-    //       .getAllConversationBetweenAttendeeAndExhibitors(this.attendeeID)
-    //       .subscribe((response) => {
-    //         // if (response.length == 0) {
-    //         //   this.isNoRecords = true;
-    //         //   return false;
-    //         // } else {
-    //         //   this.allAttendeeMsgs = response;
-    //         // }
-    //         console.log(response);
-    //       });
-    //   }
+  getAllAttendeeMessagesByType() {
+    switch (this.attendeeOption) {
+      case "0":
+        console.log("0");
+        this.service
+          .getAllAttendeeMessages(this.attendeeID)
+          .subscribe((response) => {
+            if (response.length == 0) {
+              this.isNoRecordsAttendee = true;
+              return false;
+            } else {
+              this.allAttendeeMsgs = response;
+              console.log(this.allAttendeeMsgs);
+            }
+          });
+        break;
+      case "1":
+        this.allAttendeeMsgs = [];
+        this.service
+          .getAllConversationBetweenAttendeeAndExhibitors(this.attendeeID)
+          .subscribe((response) => {
+            if (response.length == 0) {
+              this.isNoRecordsExhibitor = true;
+              return false;
+            } else {
+              this.allAttendeeExhibitorMsgs = response;
+            }
+            console.log(response);
+          });
+        break;
+      case "All":
+        this.service
+          .getAllAttendeeMessages(this.attendeeID)
+          .subscribe((response) => {
+            if (response.length == 0) {
+              this.isNoRecordsAttendee = true;
+              return false;
+            } else {
+              this.allAttendeeMsgs = response;
+              console.log(this.allAttendeeMsgs);
+            }
+          });
+
+        this.service
+          .getAllConversationBetweenAttendeeAndExhibitors(this.attendeeID)
+          .subscribe((response) => {
+            if (response.length == 0) {
+              this.isNoRecordsExhibitor = true;
+              return false;
+            } else {
+              this.allAttendeeExhibitorMsgs = response;
+            }
+            console.log(response);
+          });
+        break;
+    }
   }
 }
