2dbec1f9856325ccdae534f98243742b96b54ede
Guru Prakash Davala <guru.govindaraj@gmail.com>
2021-04-23 20:30:56 +0100

    naming conventions and code cleaning

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { AllStandsEventNamesComponent } from "./all-stands-event-names/all-stands-event-names.component";
 import { AppComponent } from "./app.component";
-import { AttendeeMessagesComponent } from "./attendee-messages/attendee-messages.component";
-import { BarChartAllTimeComponent } from "./bar-chart-all-time/bar-chart-all-time.component";
-import { BarChartViewAllComponent } from "./bar-chart-view-all/bar-chart-view-all.component";
+import { AttendeeMessagesComponent } from "./attendee/attendee-messages.component";
+import { BarChartAllTimeComponent } from "./attendee-all-bar-chart-timespent/bar-chart-all-time.component";
+import { BarChartViewAllComponent } from "./attendee-all-bar-chart-registrations/bar-chart-view-all.component";
 import { EventAttendeesCountComponent } from "./event-attendees-count/event-attendees-count.component";
 import { HomepageComponent } from "./homepage/homepage.component";
 
@@ -14,6 +15,7 @@ const routes: Routes = [
   { path: "barchartallbytime", component: BarChartAllTimeComponent },
   { path: "attendeemessages", component: AttendeeMessagesComponent },
   { path: "sessionsandattendees", component: EventAttendeesCountComponent },
+  { path: "allstands", component:AllStandsEventNamesComponent },
   { path: "**", redirectTo: "/homepage" },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,24 @@ import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { BarChartComponent } from "./bar-chart/bar-chart.component";
-
+import { BarChartComponent } from "./attendee-bar-chart-registrations/bar-chart.component";
 import { ChartsModule, ThemeService } from "ng2-charts";
 import { BubbleChartComponent } from "./bubble-chart/bubble-chart.component";
 import { LineChartComponent } from "./line-chart/line-chart.component";
-import { PieChartComponent } from "./pie-chart/pie-chart.component";
+import { PieChartComponent } from "./stands-visited-timespent/pie-chart.component";
 import { RadarChartComponent } from "./radar-chart/radar-chart.component";
-import { DoughnutChartComponent } from "./doughnut-chart/doughnut-chart.component";
-import { BarChartViewAllComponent } from './bar-chart-view-all/bar-chart-view-all.component';
-import { HeaderComponent } from './header/header.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { FooterComponent } from './footer/footer.component';
-import { AttendeeMessagesComponent } from './attendee-messages/attendee-messages.component';
+import { DoughnutChartComponent } from "./attendees-attended-registered/doughnut-chart.component";
+import { BarChartViewAllComponent } from "./attendee-all-bar-chart-registrations/bar-chart-view-all.component";
+import { HeaderComponent } from "./header/header.component";
+import { HomepageComponent } from "./homepage/homepage.component";
+import { FooterComponent } from "./footer/footer.component";
+import { AttendeeMessagesComponent } from "./attendee/attendee-messages.component";
 import { FormsModule } from "@angular/forms";
-import { BarChartTimeComponent } from './bar-chart-time/bar-chart-time.component';
-import { BarChartAllTimeComponent } from './bar-chart-all-time/bar-chart-all-time.component';
-import { EventDetailsComponent } from './event-details/event-details.component';
-import { EventAttendeesCountComponent } from './event-attendees-count/event-attendees-count.component';
+import { BarChartTimeComponent } from "./attendee-bar-chart-timespent/bar-chart-time.component";
+import { BarChartAllTimeComponent } from "./attendee-all-bar-chart-timespent/bar-chart-all-time.component";
+import { EventDetailsComponent } from "./event-details/event-details.component";
+import { EventAttendeesCountComponent } from "./event-attendees-count/event-attendees-count.component";
+import { AllStandsEventNamesComponent } from "./all-stands-event-names/all-stands-event-names.component";
 
 @NgModule({
   declarations: [
@@ -40,8 +40,15 @@ import { EventAttendeesCountComponent } from './event-attendees-count/event-atte
     BarChartAllTimeComponent,
     EventDetailsComponent,
     EventAttendeesCountComponent,
+    AllStandsEventNamesComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, ChartsModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ChartsModule,
+    HttpClientModule,
+    FormsModule,
+  ],
   providers: [ThemeService],
   bootstrap: [AppComponent],
 })
