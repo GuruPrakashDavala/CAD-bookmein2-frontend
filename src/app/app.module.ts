a2c754b6f4c94820de4d3c23a91501f79e7655d9
Guru Prakash Davala <guru.govindaraj@gmail.com>
2021-04-27 11:38:45 +0100

    removed unused files and added comments for APIs in service file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,7 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BarChartComponent } from "./attendee-bar-chart-registrations/bar-chart.component";
 import { ChartsModule, ThemeService } from "ng2-charts";
-import { BubbleChartComponent } from "./bubble-chart/bubble-chart.component";
-import { LineChartComponent } from "./line-chart/line-chart.component";
 import { PieChartComponent } from "./stands-visited-timespent/pie-chart.component";
-import { RadarChartComponent } from "./radar-chart/radar-chart.component";
 import { DoughnutChartComponent } from "./attendees-attended-registered/doughnut-chart.component";
 import { BarChartViewAllComponent } from "./attendee-all-bar-chart-registrations/bar-chart-view-all.component";
 import { HeaderComponent } from "./header/header.component";
@@ -18,7 +15,6 @@ import { AttendeeMessagesComponent } from "./attendee/attendee-messages.componen
 import { FormsModule } from "@angular/forms";
 import { BarChartTimeComponent } from "./attendee-bar-chart-timespent/bar-chart-time.component";
 import { BarChartAllTimeComponent } from "./attendee-all-bar-chart-timespent/bar-chart-all-time.component";
-import { EventDetailsComponent } from "./event-details/event-details.component";
 import { EventAttendeesCountComponent } from "./event-attendees-count/event-attendees-count.component";
 import { AllStandsEventNamesComponent } from "./all-stands-event-names/all-stands-event-names.component";
 
@@ -26,10 +22,7 @@ import { AllStandsEventNamesComponent } from "./all-stands-event-names/all-stand
   declarations: [
     AppComponent,
     BarChartComponent,
-    BubbleChartComponent,
-    LineChartComponent,
     PieChartComponent,
-    RadarChartComponent,
     DoughnutChartComponent,
     BarChartViewAllComponent,
     HeaderComponent,
@@ -38,7 +31,6 @@ import { AllStandsEventNamesComponent } from "./all-stands-event-names/all-stand
     AttendeeMessagesComponent,
     BarChartTimeComponent,
     BarChartAllTimeComponent,
-    EventDetailsComponent,
     EventAttendeesCountComponent,
     AllStandsEventNamesComponent,
   ],
--- src/app/services/bookmein2-api.service.ts
+++ src/app/services/bookmein2-api.service.ts
@@ -7,26 +7,33 @@ import { HttpClient, HttpParams } from "@angular/common/http";
 export class Bookmein2APIService {
   constructor(public http: HttpClient) {}
 
+  //API for popular sessions based on registrations
   getPopularSessions() {
     return this.http.get<[]>("http://localhost:3000/eventregistrations");
   }
 
+  //API for popular sessions based on total time spent
   getPopularSessionsByTimeSpent() {
     return this.http.get<[]>("http://localhost:3000/geteventsbytotaltimespent");
   }
 
+  //API to get single attendee details based on the route param
   getAttendeeDetails(attendeeID: number) {
     return this.http.get<[]>(
       "http://localhost:3000/getattendeedetails/" + attendeeID
     );
   }
 
+  //API to get all attendee-attendee conversations - based on the route param
+  //Get All messages by attendeeID
   getAllAttendeeMessages(attendeeID: number) {
     return this.http.get<[]>(
       "http://localhost:3000/getallattendeesmessages/" + attendeeID
     );
   }
 
+  //API to get all conversation between attendee and exhibior - based on the route param
+  //Get All messages by attendeeID
   getAllConversationBetweenAttendeeAndExhibitors(attendeeID: number) {
     return this.http.get<[]>(
       "http://localhost:3000/getallexhibitorsanddelegatesconversation/" +
@@ -34,18 +41,21 @@ export class Bookmein2APIService {
     );
   }
 
+  //API to fetch all events attended by attendee - based on the route param
   geetAllEventsofAttendee(attendeeID: number) {
     return this.http.get<[]>(
       "http://localhost:3000/getalleventsattendedbyattendee/" + attendeeID
     );
   }
 
+  //API to get total attendees registered
   getTotalAttendeesRegistered() {
     return this.http.get<[{ totalregistrations: number }]>(
       "http://localhost:3000/totalregistered"
     );
   }
 
+  //API to get total attendees attended
   getTotalAttendeesAttended() {
     return this.http.get<[{ totalattended: number }]>(
       "http://localhost:3000/totalattended"
@@ -57,6 +67,7 @@ export class Bookmein2APIService {
     return this.http.get<[]>("http://localhost:3000/attendeesgroupedbyeventid");
   }
 
+  //API to get total stands visited and duration spent
   getStandsVisitedAndDuration() {
     return this.http.get<[{ eventid: number; totaltime: number }]>(
       "http://localhost:3000/standsvisitedandduration"
